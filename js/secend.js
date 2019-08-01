fca0a986fa5ce0e869e238b984018568fd780747
booolink <aeze716.james@gmail.com>
2019-08-01 18:14:37 +0800

    20190801y

---
+++ js/secend.js
@@ -0,0 +1,38 @@
+
+
+
+var one = document.querySelector("#one");
+var two = document.querySelector("#two");
+
+$(".menu_2").click(function () {
+    password();
+});
+function password() {
+    var pass = prompt("請輸入訪問密碼", "");
+    if (pass == "123") {
+        one.style.display = "block";
+    } else if (pass != null && pass != "") {
+        alert("錯誤");
+        one.style.display = "none";
+        location.reload();
+    }
+}
+
+$(".menu_3").click(function () {
+    if (one.style.display == "block") {
+        two.style.display = "block";
+        one.style.display = "none";
+    } else {
+        two.style.display = "block";
+    }
+})
+
+$(".menu_2").click(function () {
+    if (two.style.display == "block") {
+        two.style.display = "none";
+    } else {
+        two.style.display == "block";
+    }
+})
+
+
