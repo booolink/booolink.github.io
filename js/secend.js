d13231c9828da2ad9f3e3f4938b9d1a89c353392
booolink <aeze716.james@gmail.com>
2019-08-01 18:54:54 +0800

    20190801op

--- js/secend.js
+++ js/secend.js
@@ -9,7 +9,7 @@ $(".menu_2").click(function () {
 });
 function password() {
     var pass = prompt("請輸入訪問密碼", "");
-    if (pass == "123") {
+    if (pass == "55554444") {
         one.style.display = "block";
     } else if (pass != null && pass != "") {
         alert("錯誤");
