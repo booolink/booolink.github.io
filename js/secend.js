b646f52221e0b3434b9944c4bb53b5668efe8e2d
booolink <aeze716.james@gmail.com>
2019-08-02 14:41:20 +0800

    20190802b

--- js/secend.js
+++ js/secend.js
@@ -3,12 +3,14 @@
 
 var one = document.querySelector("#one");
 var two = document.querySelector("#two");
+var three = document.querySelector("#three");
 
 $(".menu_2").click(function () {
     password();
 });
+
 function password() {
-    var pass = prompt("請輸入訪問密碼", "");
+    var pass = prompt("請輸入訪問密碼", "55554444");
     if (pass == "55554444") {
         one.style.display = "block";
     } else if (pass != null && pass != "") {
@@ -18,21 +20,38 @@ function password() {
     }
 }
 
+//公司
+$(".menu_2").click(function () {
+    if (two.style.display == "block") {
+        two.style.display = "none";
+    } else if (three.style.display = "block") {
+        three.style.display = "none";
+    }
+})
+
+//一般
 $(".menu_3").click(function () {
     if (one.style.display == "block") {
         two.style.display = "block";
         one.style.display = "none";
-    } else {
+    } else if (three.style.display = "block") {
         two.style.display = "block";
+        three.style.display = "none";
     }
 })
 
-$(".menu_2").click(function () {
+//小程式
+$(".menu_4").click(function () {
     if (two.style.display == "block") {
         two.style.display = "none";
+        three.style.display = "block";
+    } else if (one.style.display == "block") {
+        one.style.display = "none";
+        three.style.display = "block";
     } else {
-        two.style.display == "block";
+        three.style.display = "block";
     }
+
 })
 
 
