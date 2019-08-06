42dddd3b7a7dc07a35c8c19e91759bfdc9595409
booolink <aeze716.james@gmail.com>
2019-08-06 12:39:05 +0800

    20190806a

---
+++ js/data.js
@@ -0,0 +1,25 @@
+let data = {
+  one: [
+    { date: "2019/07/27", title: "", link: "https://booolink.github.io/works/20190724_SPORT_james/index.html" },
+    { date: "2019/07/27", title: "", link: "https://booolink.github.io/works/20190725_goldfunnel_james/index.html" },
+    { date: "2019/07/27", title: "", link: "https://booolink.github.io/works/20190725_sport795_james/index.html" },
+    { date: "2019/07/28", title: "", link: "https://booolink.github.io/works/20190727_88Game_james/index.html" },
+    { date: "2019/07/28", title: "", link: "https://booolink.github.io/works/20190729_FootBasketball_james/index.html" },
+    { date: "2019/07/28", title: "", link: "https://booolink.github.io/works/20190729_Jinsha_james/index.html" },
+    { date: "2019/07/28", title: "", link: "https://booolink.github.io/works/20190729_goldfunnel-2_james/index.html" },
+  ],
+  two: [
+    { date: "2019/05/18", title: "專題", link: "https://booolink.github.io/chinchin/index.html" },
+    { date: "2019/05/27", title: "micsoft", link: "https://booolink.github.io/micsoft/Bootstrap/index.html" },
+  ],
+  three: [
+    { date: "2019/06/06", title: "Tictac", link: "https://booolink.github.io/PlayAGame/Tictac/index.html" },
+    { date: "2019/07/03", title: "taiwan svg", link: "https://booolink.github.io/Function/20190703svg/index.html" },
+    { date: "2019/06/21", title: "隨機亂數", link: "https://booolink.github.io/Function/隨機亂數.html" },
+    { date: "2019/08/05", title: "canvas", link: "https://booolink.github.io/PlayAGame/plan_1/index.html" },
+
+  ]
+
+}
+
+
--- js/secend.js
+++ js/secend.js
@@ -1,57 +1,68 @@
 
+  let vm = new Vue({
+    el: "#page2",
+    data: data,
+  
+    methods: {
+  
+      //公司
+      password() {
+        var one = document.querySelector("#one");
+        var two = document.querySelector("#two");
+        var three = document.querySelector("#three");
+        var pass = prompt("請輸入訪問密碼", "");
+        //密碼設定
+        if (pass == "55554444") {
+          one.style.display = "block";
+        } else if (pass != null && pass != "") {
+          alert("錯誤");
+          one.style.display = "none";
+          location.reload();
+        }
+  
+        if (two.style.display == "block") {
+          two.style.display = "none";
+        } else if (three.style.display = "block") {
+          three.style.display = "none";
+        }
+      },//---------公司
+  
+      //一般
+      numbertwo() {
+        var one = document.querySelector("#one");
+        var two = document.querySelector("#two");
+        var three = document.querySelector("#three");
+  
+        if (one.style.display == "block") {
+          two.style.display = "block";
+          one.style.display = "none";
+        } else if (three.style.display = "block") {
+          two.style.display = "block";
+          three.style.display = "none";
+        }
+      },//-------一般
+  
+      //小程式
+      numberthree() {
+        var one = document.querySelector("#one");
+        var two = document.querySelector("#two");
+        var three = document.querySelector("#three");
+  
+        if (two.style.display == "block") {
+          two.style.display = "none";
+          three.style.display = "block";
+        } else if (one.style.display == "block") {
+          one.style.display = "none";
+          three.style.display = "block";
+        } else {
+          three.style.display = "block";
+        }
+      },//---------小程式
+  
 
 
-var one = document.querySelector("#one");
-var two = document.querySelector("#two");
-var three = document.querySelector("#three");
-
-$(".menu_2").click(function () {
-    password();
-});
-
-function password() {
-    var pass = prompt("請輸入訪問密碼", "");
-    if (pass == "55554444") {
-        one.style.display = "block";
-    } else if (pass != null && pass != "") {
-        alert("錯誤");
-        one.style.display = "none";
-        location.reload();
-    }
-}
-
-//公司
-$(".menu_2").click(function () {
-    if (two.style.display == "block") {
-        two.style.display = "none";
-    } else if (three.style.display = "block") {
-        three.style.display = "none";
-    }
-})
-
-//一般
-$(".menu_3").click(function () {
-    if (one.style.display == "block") {
-        two.style.display = "block";
-        one.style.display = "none";
-    } else if (three.style.display = "block") {
-        two.style.display = "block";
-        three.style.display = "none";
-    }
-})
-
-//小程式
-$(".menu_4").click(function () {
-    if (two.style.display == "block") {
-        two.style.display = "none";
-        three.style.display = "block";
-    } else if (one.style.display == "block") {
-        one.style.display = "none";
-        three.style.display = "block";
-    } else {
-        three.style.display = "block";
-    }
-
-})
-
-
+    }//methods
+  })
+  
+  
+  
